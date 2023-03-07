bced3be5868916718b0b30c3b0fd00b31fbca7a9
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-03-08 00:25:55 +0900

    add

--- AssociatedEX/ViewController.swift
+++ AssociatedEX/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     var coustom: CustomArray<Any>!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = URL(string: "https://umayadia-apisample.azurewebsites.net/api/persons/Shakespeare")
+
+        ApiManager.shared.callApi(url: url, type: Model.self) { result in
+            switch result {
+            case .success(let success):
+                print(success)
+            case .failure(let failure):
+                print(failure, ":", failure._code)
+            }
+        }
+
         coustom = CustomArray<Any>()
         for index in 0...10 {
             coustom.append(item: "item_index: \(index)")
